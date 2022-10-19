41cfa6df77e3957725d9167d30c2f36cbd9c1de5
JBSanMarcos <116197791+JBSanMarcos@users.noreply.github.com>
2022-10-19 12:35:32 -0500

    Add files via upload

---
+++ functions/lib/index.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+exports.initUserInCloudDb = functions.auth.user().onCreate((user) => {
+    const userId = user.uid;
+    const db = admin.firestore();
+    const data = {
+        education: [],
+        experience: [],
+        skillset: {
+            languages: {
+                main: [],
+                second: []
+            },
+            others: {
+                main: [],
+                second: []
+            }
+        },
+        name: '',
+        personalNote: '',
+        professionalExpectations: '',
+        title: '',
+        uid: userId,
+        email: user.email
+    };
+    return db.collection('users').doc(userId).set(data);
+});
+//# sourceMappingURL=index.js.map
---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+exports.initUserInCloudDb = functions.auth.user().onCreate((user) => {
+  const userId = user.uid;
+  const db = admin.firestore();
+
+  const data = {
+    education: [],
+    experience: [],
+    skillset: {
+      languages: {
+        main: [],
+        second: []
+      },
+      others: {
+        main: [],
+        second: []
+      }
+    },
+    name: '',
+    personalNote: '',
+    professionalExpectations: '',
+    title: '',
+    uid: userId,
+    email: user.email
+  }
+
+  return db.collection('users').doc(userId).set(data);
+});
